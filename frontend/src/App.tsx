3da06c5ec5d374945215b4474da9ea740e3c9b9b
NewarkX <brunowmanha@gmail.com>
2022-01-10 09:59:16 -0300

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <h1>Hello world</h1>
-=======
 import React from 'react';
 
 
 function App() {
   return (
     <h1>Página principal</h1>
->>>>>>> 10f36f37a414f02239265547af50a6bff617a9ad
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-
->>>>>>> 10f36f37a414f02239265547af50a6bff617a9ad
 
 ReactDOM.render(
   <React.StrictMode>
